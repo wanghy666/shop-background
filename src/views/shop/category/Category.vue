f9ee6bf69a6f8e29748b0552c43fc94a65ee0eb3
Alisa-ying <1038152156@qq.com>
2020-05-06 17:51:07 +0800

    test

--- src/views/shop/category/Category.vue
+++ src/views/shop/category/Category.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="category-wrap">
-    master分支
-    1111
     <div class="border-bottom p-2">
       <el-button @click="createTop" size="mini" style="background-color:teal;color:white">创建顶级分类</el-button>
     </div>
